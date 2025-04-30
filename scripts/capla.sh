bfbdc8557253f6cd12072036edc0928875622155
Giorgio Vinciguerra <i@gvdev.com>
2025-04-30 11:12:01 +0200

    Simplify script arguments, handle Python dependencies

--- scripts/capla.sh
+++ scripts/capla.sh
@@ -1,13 +1,38 @@
-BIN_PATH="$1"
-MKSARY_PATH="$2"
-SRC_PATH="$3"
-KMER="$4"
-# OUTPUT_FILE="$5"
+SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+BIN_PATH="$SCRIPT_PATH/../build"
+MKSARY_PATH="$BIN_PATH/mksary"
+SRC_PATH="$SCRIPT_PATH/../src"
+KMER="${2:-21}"
+
+if [[ -z "$1" ]]; then
+    echo "Usage: $0 <directory> [kmer]"
+    echo "  directory: Directory containing the .fna or .fasta files."
+    echo "  kmer: Optional k-mer size (default is 21)."
+    exit 1
+fi
+
+if [[ -d "$1" ]]; then
+    cd "$1" || exit 1
+else
+    echo "Directory $1 does not exist."
+    exit 1
+fi
+
+if [[ ! -d "$BIN_PATH/venv" ]]; then
+    echo "Creating virtual environment in $BIN_PATH/venv"
+    python3 -m venv "$BIN_PATH/venv"
+    source "$BIN_PATH/venv/bin/activate"
+    python3 -m pip install "pandas>=2.2.3" "scipy>=1.15.2"
+else
+    source "$BIN_PATH/venv/bin/activate"
+fi
 
 count_processed=0
 count_sa=0
 count_seg=0
 count_capla=0
+
+shopt -s nullglob # Do not match empty glob patterns
 
 for file in *.fna *.fasta; do
     [[ -e "$file" ]] || continue  # skip if no matching files
